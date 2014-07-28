46956ec08934e2740e8939f420873fddb11d37f0
Reza <reza@mynd.us>
2014-07-28 06:46:16 -0700

    Started decoupling UI from users selections

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,5 +1,3 @@
-var sudoku = sudoku || {};
-
 function Game(){
   this.board = new Board();
 };
@@ -18,6 +16,7 @@ function Board (){
     this.rows = [];
     this.cols = [];
     this.secs = [];
+    this.tileValues = [];
 };
 
 Board.prototype.generate = function(){
@@ -31,7 +30,16 @@ Board.prototype.generate = function(){
         current_col = 0,
         current_sec = 0,
         z = 0,
-        a = 0;
+        a = 0,
+        _board = this;
+
+    function generateBackHtml(){
+        var html = '';
+        for (var x = 1; x <= 9;x++){
+            html += '<span class="digitBtn digitBtn' + x + '" data-value="'+ x + '"><span class="digit">' + x +  '</span></span>';
+        }
+        return html;
+    }
 
     for(;x < 9;x ++){
         section_string = '<section>';
@@ -40,8 +48,9 @@ Board.prototype.generate = function(){
             current_row = (Math.floor(x / 3) * 3) + (Math.floor(y / 3));
             current_col = z + (a * 3);
             z = (z < 2) ? z + 1 : 0;
-            section_string += '<div data-row="' + current_row + '" data-col="' + current_col + '" data-sec="' + current_sec + '" class="tile" id="cell' + cell +'"><div class="tile-wrapper"><div class="front"><div class="front-wrapper"></div></div><div class="back">'+ backHTML + '</div></div></div>';
+            section_string += '<div data-row="' + current_row + '" data-col="' + current_col + '" data-sec="' + current_sec + '" class="tile" data-idx="' + cell +'"><div class="tile-wrapper"><div class="front"><div class="front-wrapper"></div></div><div class="back">'+ backHTML + '</div></div></div>';
             cell++;
+            _board.tileValues.push(null);
         }
         y = 0;
         a = (a < 2) ? a + 1 : 0;
@@ -53,43 +62,74 @@ Board.prototype.generate = function(){
 };
 
 Board.prototype.userEvents = function(){
-    var _board = this;
+    var _board = this,
+        active_event = 'ontouchstart' in document.documentElement ? 'touchstart' : 'click';
+
     function removeFrom(type, idx, val) {
         _board[type][idx].each(function(){
             $(this).find('.digitBtn' + val).addClass('disabled');
-        })
+        });
     };
 
-    $('.digitBtn').click(function(){
-        var $t = $(this);
-        if(!$t.hasClass('disabled')) {
-            var val = $t.data('value'),
-                tile = $t.parents('.tile'),
-                cur_row = parseFloat(tile.data('row')),
-                cur_sec = parseFloat(tile.data('sec')),
-                cur_col = parseFloat(tile.data('col'));
+    function addTo(type, idx, val){
+        _board[type][idx].each(function(){
+            $(this).find('.digitBtn' + val).addClass('disabled');
+        });
+    }
 
-            removeFrom('rows', cur_row, val);
-            removeFrom('cols', cur_col, val);
-            removeFrom('secs', cur_sec, val);
+    function onResize() {
+        $('#wrapper').css('width', $('img').width());
+    };
 
-            tile.find('.front-wrapper').text(val);
+    function onBoardChange(evt) {
+      if(_board.tileValues[evt.idx] == null) {
+            removeFrom('rows', evt.cur_row, evt.val);
+            removeFrom('cols', evt.cur_col, evt.val);
+            removeFrom('secs', evt.cur_sec, evt.val);
+            _board.tileValues[evt.idx] = evt.val;
+            evt.$tile.find('.front-wrapper').text(evt.val);
+      } else {
+          addTo('rows', evt.cur_row, evt.val);
+          addTo('cols', evt.cur_col, evt.val);
+          addTo('secs', evt.cur_sec, evt.val);
+      }
+    };
+
+    $('.tile').on(active_event, function(){
+       var $t = $(this);
+
+        if(!$t.hasClass('active')){
+            $('.active').removeClass('active');
+            $t.addClass('active');
+        } else {
+            $('.active').removeClass('active');
         }
     });
+
+    $('.digitBtn').on(active_event, function(){
+        var $t = $(this),
+            $tile = $t.parents('.tile');
+
+        if(!$t.hasClass('disabled')) {
+            data = {
+                $el: $t,
+                idx: parseFloat($tile.attr('data-idx')),
+                val: $t.data('value'),
+                $tile: $tile,
+                cur_row: parseFloat($tile.data('row')),
+                cur_sec: parseFloat($tile.data('sec')),
+                cur_col: parseFloat($tile.data('col'))
+            };
+            onBoardChange(data);
+        }
+    });
+
+    $(window).on('resize', function(){
+       onResize();
+    });
+
 };
 
-(function(){
-    $('#wrapper').css('width', $('img').width());
-    var game = new Game();
-    game.init();
-})();
 
 
 
-function generateBackHtml(){
-    var html = '';
-    for (var x = 1; x <= 9;x++){
-        html += '<span class="digitBtn digitBtn' + x + '" data-value="'+ x + '"><span class="digit">' + x +  '</span></span>';
-    }
-    return html;
-}
